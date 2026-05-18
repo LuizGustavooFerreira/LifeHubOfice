b7cb6e65426f7121f6ec07a15afb0296ff39877b
LuizGustavooFerreira <Luizgustavoferreira135@gmail.com>
2026-05-18 20:47:16 -0300

    add gunicorn

--- FrontEnd/src/api/api.js
+++ FrontEnd/src/api/api.js
@@ -1,33 +1,40 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/"
+  baseURL: "https://lifehubofice-production.up.railway.app/api/"
 });
 
 // Adiciona token JWT a todas as requisições
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("access_token");
+
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
+
     return config;
   },
+
   (error) => {
     return Promise.reject(error);
   }
 );
 
-// Trata erros (e.g., token expirado)
+// Trata erros
 api.interceptors.response.use(
   (response) => response,
+
   (error) => {
+
     if (error.response?.status === 401) {
-      // Token expirado ou inválido
+
       localStorage.removeItem("access_token");
       localStorage.removeItem("refresh_token");
-      window.location.href = "/usuario/login";
+
+      window.location.href = "/";
     }
+
     return Promise.reject(error);
   }
 );
