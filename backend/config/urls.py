aa5a875cd5bd0f0a4d64bbb8b0cc5801c7ce8ec0
LuizGustavooFerreira <Luizgustavoferreira135@gmail.com>
2026-05-18 23:23:04 -0300

    fix render static routing

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework_simplejwt.views import TokenRefreshView
 from app.views import MeuTokenView
 from django.views.generic import TemplateView
@@ -7,7 +7,8 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('app.urls')),
-    path("", TemplateView.as_view(template_name="index.html")),
     path('api/token/', MeuTokenView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    re_path(r'^(?!assets/).*$', TemplateView.as_view(template_name='index.html')),
 ]
