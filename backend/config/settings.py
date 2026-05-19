95020587a13bd8054fafb239c99b6fab60e6ce5f
LuizGustavooFerreira <Luizgustavoferreira135@gmail.com>
2026-05-18 22:16:54 -0300

    add dj database url

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -114,7 +114,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "../FrontEnd/dist/assets")
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenRefreshView
 from app.views import MeuTokenView
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('app.urls')),
-
+    path("", TemplateView.as_view(template_name="index.html")),
     path('api/token/', MeuTokenView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
