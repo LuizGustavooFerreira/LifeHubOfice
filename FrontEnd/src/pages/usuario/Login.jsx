93b3a6d1314c3baa5828b16ed04f672970951ae4
LuizGustavooFerreira <Luizgustavoferreira135@gmail.com>
2026-05-18 23:46:38 -0300

    fix localhost references

--- FrontEnd/src/pages/usuario/Login.jsx
+++ FrontEnd/src/pages/usuario/Login.jsx
@@ -15,7 +15,7 @@ export default function Login() {
     setCarregando(true);
 
     try {
-      const resposta = await fetch(""/api""/api"/token/", {
+      const resposta = await fetch("/api/token/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
